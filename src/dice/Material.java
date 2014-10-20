04da9c1cb875929098ef14ab3c5df140dd66ce88
Lyle Cleveland <lcleveland@herzing.edu>
2014-10-20 18:28:50 -0500

    added more materials

--- src/dice/Material.java
+++ src/dice/Material.java
@@ -6,6 +6,8 @@ public final class Material {
 	public static final String METAL = "Metal";
 	public static final String PLASTIC = "Plastic";
 	public static final String LEATHER = "Leather";
+	public static final String BONE = "Bone";
+	public static final String IVORY = "Ivory";
 
 	private Material() {
 	}
